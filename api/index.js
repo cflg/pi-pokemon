c0887b68304477eb8f0c0a86ca7bfb9140bfa3a7
Cristian Lg <cristianflg1@hotmail.com>
2022-11-19 01:11:55 -0300

    a ver ahora anda

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-
+const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(process.env.PORT || 3001 , () => {
+  server.listen(PORT, () => {
     console.log('Vas bien pero falta.'); // eslint-disable-line no-console
   });
 });
