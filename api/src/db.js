40c789118e4d7370e9190c816aad799d201eaa33
Cristian Lg <cristianflg1@hotmail.com>
2022-11-21 16:53:17 -0300

    asdas

--- api/src/db.js
+++ api/src/db.js
@@ -3,16 +3,42 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const pg = require('pg')
-/* const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB
-} = process.env;
- */
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } = process.env
 
-const sequelize = new Sequelize(`${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: DB_PORT,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+    : new Sequelize(
+      `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+      { logging: false, native: false }
+    );
+
+/* const sequelize = new Sequelize(`${process.env.DB}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
   
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+}); */
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
